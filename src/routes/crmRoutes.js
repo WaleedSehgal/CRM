1b9ab1be3fcd8b0e8d17fe1e16a7d9f7773a03b4
waleed sehgal <waleedsehgal@gmail.com>
2019-06-28 02:56:52 -0700

    Add MVC structure

---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,23 @@
+const routes = (app) => {
+    app.route("/contact")
+    .get((req, res, next)=> {
+        // middleware
+        console.log(`Request from" ${req.orignalUrl}`)
+        console.log(`Request from" ${req.method}`)
+        next();
+    }, (req, res, next) => {
+        res.send("GET request successfull!");
+    })
+    
+    .post((req, res) => 
+    res.send("POST request successfull!"));
+
+    app.route("/contact/:contactId")
+    .put((req, res)=>
+    res.send("PUT request successfull!"))
+
+    .delete((req, res) => 
+    res.send("DELETE request successfull!"));
+}
+
+export default routes;
